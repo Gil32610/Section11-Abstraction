322fb956f535bb13e86a1673cced210ac6a2b39a
GIL32610 <gil.00000032610@unicap.br>
2024-04-02 18:10:48 -0300

    Abstract class fields and methods

---
+++ src/dev/lpa/abstractclasses/Dog.java
@@ -0,0 +1,17 @@
+package dev.lpa.abstractclasses;
+
+public class Dog extends Animal{
+    public Dog(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+
+    @Override
+    public void move(String speed) {
+
+    }
+
+    @Override
+    public void makeNoise() {
+
+    }
+}
---
+++ src/dev/lpa/abstractclasses/Main.java
@@ -0,0 +1,4 @@
+package dev.lpa.abstractclasses;
+
+public class Main {
+}
